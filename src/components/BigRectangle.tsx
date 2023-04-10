8415c2ad6ab41e32f8bdffc88aabe19fecaf8f39
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-04-10 11:42:32 -0400

    Created BigRectangle component. Modified NavBar component.

---
+++ src/components/BigRectangle.tsx
@@ -0,0 +1,9 @@
+function BigRectangle() {
+  return (
+    <div className="big-rectangle">
+      <img src="" alt="Image of Gabriel Cordero" />
+    </div>
+  )
+}
+
+export default BigRectangle
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,29 +1,60 @@
-import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/esm/Navbar';
 import './componentsStyle.css'
+import { useState } from 'react';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/esm/NavDropdown';
 
 const NavBar = () => {
   return (
-    <Nav className='d-flex justify-content-between nav-bar'>
+    <Navbar className='d-flex flex-row-reverse justify-content-between nav-bar'
+      sticky="top" expand="sm" collapseOnSelect>
 
-      <div className='d-inline-flex p-5'>
-          <Nav.Item>
-            <Nav.Link className='text-darkGray' href='#'>About me</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link className='text-darkGray' href='#'>Portfolio</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link className='text-darkGray' href='#'>More</Nav.Link>
-          </Nav.Item>
-      </div>
+      <Navbar.Brand>
+         <div className='nav-circle bg-clearWhite'>
+             <Nav.Link className='text-lightBlack' href='#'>Contact me</Nav.Link>
+        </div>
+      </Navbar.Brand>
 
-      <div className='nav-circle bg-clearWhite p-5'>
-          <Nav.Item>
-            <Nav.Link className='text-lightBlack' href='#'>Contact me</Nav.Link>
-          </Nav.Item>
-      </div>
-    </Nav>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav>
+          <div className='nav-item-top bg-clearWhite'>
+            <Nav.Link className='text-darkGray px-4' href='#'>About me</Nav.Link>
+          </div>
+          <div className='nav-item bg-clearWhite'>
+            <Nav.Link className='text-darkGray px-4' href='#'>Portfolio</Nav.Link>
+          </div>
+          <div className='nav-item-bottom bg-clearWhite'>
+            <Nav.Link className='text-darkGray px-4' href='#'>More</Nav.Link>
+          </div>
+        </Nav>
+      </Navbar.Collapse>
+
+    </Navbar>
   )
 }
 
 export default NavBar
+
+
+
+    // <Nav className='d-flex justify-content-between nav-bar'>
+
+    //   <div className='d-inline-flex'>
+    //       <Nav.Item>
+    //         <Nav.Link className='text-darkGray' href='#'>About me</Nav.Link>
+    //       </Nav.Item>
+    //       <Nav.Item>
+    //         <Nav.Link className='text-darkGray' href='#'>Portfolio</Nav.Link>
+    //       </Nav.Item>
+    //       <Nav.Item>
+    //         <Nav.Link className='text-darkGray' href='#'>More</Nav.Link>
+    //       </Nav.Item>
+    //   </div>
+
+    //   <div className='nav-circle bg-clearWhite'>
+    //       <Nav.Item>
+    //         <Nav.Link className='text-lightBlack' href='#'>Contact me</Nav.Link>
+    //       </Nav.Item>
+    //   </div>
+    // </Nav>
