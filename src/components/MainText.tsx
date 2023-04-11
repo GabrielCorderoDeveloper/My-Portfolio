8134daaef9ddd94b15475099e396e506ef93b505
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-04-11 08:30:28 -0400

    Added translucid white background. To the component MainText.

--- src/components/MainText.tsx
+++ src/components/MainText.tsx
@@ -1,15 +1,12 @@
-import React from 'react'
-
 const MainText = () => {
   return (
-    <div className='main-container'>
-        <div className='d-flex flex-column justify-content-start '>
-          <p className='title text-lightBlack'>My name <br/>is <b className='roboto'>Gabriel</b></p>
-          <p className='paragraph text-darkGray col-lg-3 col-md-5  col-9'>I am an extremely dedicated person following his dream of become a Front End developer.</p>
-        </div>
-
-        <button className='bg-mainColor button text-clearWhite'>Hire me</button>
-    </div>
+      <div className='main-container col-lg-3 col-md-5 col-9'>
+          <div className='d-flex flex-column justify-content-start white-transparent-bg'>
+            <p className='title text-lightBlack'>My name <br/>is <b className='roboto'>Gabriel</b></p>
+            <p className='paragraph text-darkGray'>I am an extremely dedicated person following his dream of become a Front End developer.</p>
+          </div>
+          <button className='bg-mainColor button text-clearWhite'>Hire me</button>
+      </div>
   )
 }
 
