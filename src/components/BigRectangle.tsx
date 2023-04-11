a9c2f93cfe9d0ac9120dcfca70d6816b8e4c9136
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-04-10 21:36:39 -0400

    Aded favicon. Fixed bugs related to small screens. Improved BigRectangle element.

--- src/components/BigRectangle.tsx
+++ src/components/BigRectangle.tsx
@@ -1,7 +1,8 @@
 function BigRectangle() {
   return (
     <div className="big-rectangle">
-      <img src="" alt="Image of Gabriel Cordero" />
+      <img src="/src/assets/gray_reactangle.png" alt="Big rotated rectangle" />
+      <img src="/src/assets/gabriel_image.png" alt="Image of Gabriel Cordero" />
     </div>
   )
 }
--- src/components/MainText.tsx
+++ src/components/MainText.tsx
@@ -5,7 +5,7 @@ const MainText = () => {
     <div className='main-container'>
         <div className='d-flex flex-column justify-content-start '>
           <p className='title text-lightBlack'>My name <br/>is <b className='roboto'>Gabriel</b></p>
-          <p className='paragraph text-darkGray col-3'>I am an extremely dedicated person following his dream of become a Front End developer.</p>
+          <p className='paragraph text-darkGray col-lg-3 col-md-5  col-9'>I am an extremely dedicated person following his dream of become a Front End developer.</p>
         </div>
 
         <button className='bg-mainColor button text-clearWhite'>Hire me</button>
