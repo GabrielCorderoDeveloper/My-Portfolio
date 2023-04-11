846b7e84e55f7af58876eaaae6b7b8047d02432a
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-04-11 12:00:18 -0400

    SocialMedia now opens a new tab if you click.

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -4,18 +4,36 @@ interface Props {
     color: string;
     background: boolean;
 }
+
 const SocialMedia = ({ color, background }: Props) => {
+    //Default styling for all the icons.
     const style = {
         color: color,
         fontSize: "50px",
     }
 
+    /*These links will be used on each icon to open a new tab with the indicated link
+    if clicked. */
+    type Links = {
+      youtube: string;
+      instagram: string;
+      github: string;
+      linkedin: string;
+    };
+
+    const links: Links = {
+      youtube: 'https://www.youtube.com/channel/UC70Va2m-AMFg7djlFzVuM9A',
+      instagram: 'https://www.instagram.com/gabriel_coder47/',
+      github: 'https://github.com/GabrielCorderoDeveloper',
+      linkedin: 'https://www.linkedin.com/in/gabriel-cordero-0960b9244/',
+    }
+
   return (
     <div className={ background ? "social social-bg" : 'social'}>
-        <a href="#"><AiFillYoutube style={style}/></a>
-        <a href="#"><AiOutlineInstagram style={style}/></a>
-        <a href="#"><AiFillGithub style={style}/></a>
-        <a href="#"><AiFillLinkedin style={style}/></a>
+        <a target="_blank" href={links.youtube}><AiFillYoutube style={style}/></a>
+        <a target="_blank" href={links.instagram}><AiOutlineInstagram style={style}/></a>
+        <a target="_blank" href={links.github}><AiFillGithub style={style}/></a>
+        <a target="_blank" href={links.linkedin}><AiFillLinkedin style={style}/></a>
     </div>
   );
 }
