522b09b78914ceceb155cbc69f3a498caa2fa0a5
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-04-19 14:43:28 -0400

    Removed strict mode.

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,5 @@ import './index.css'
 import '../css/main.min.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
 )
