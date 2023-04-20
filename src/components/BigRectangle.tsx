54789bce416c83c015834dad47c56ec1c781cad3
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-04-20 15:37:19 -0400

    Updated BigRectangle images.

--- src/components/BigRectangle.tsx
+++ src/components/BigRectangle.tsx
@@ -3,8 +3,8 @@ import './BigRectangle.css';
 function BigRectangle() {
   return (
     <div className="big-rectangle">
-      <img src="/assets/gray_reactangle.png" alt="Big rotated rectangle" />
-      <img src="/assets/gabriel_image.png" alt="Image of Gabriel Cordero" />
+      <img src="./assets/gray_reactangle.png" alt="Big rotated rectangle" />
+      <img src="../assets/gabriel_image.png" alt="Image of Gabriel Cordero" />
     </div>
   )
 }
