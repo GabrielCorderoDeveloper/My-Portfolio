7c330b418f6db5bd5b6829190a94af71c2c9d3e5
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-04-20 13:46:10 -0400

    Added image ALT on Projects component.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,29 +4,34 @@ const Projects = () => {
 
   const data = [
     {
-      link: "https://www.google.com",
+      link: "https://github.com/GabrielCorderoDeveloper",
       text: "How to afford a tesla",
-      image: "https://tesla-cdn.thron.com/delivery/public/image/tesla/c82315a6-ac99-464a-a753-c26bc0fb647d/bvlatuR/std/1200x628/lhd-model-3-social"
+      image: "https://tesla-cdn.thron.com/delivery/public/image/tesla/c82315a6-ac99-464a-a753-c26bc0fb647d/bvlatuR/std/1200x628/lhd-model-3-social",
+      alt: "How to afford a tesla"
     },
     {
-      link: "https://www.facebook.com",
-      text: "Facebook",
-      image: "https://via.placeholder.com/150"
+      link: "https://github.com/GabrielCorderoDeveloper",
+      text: "Coming soon",
+      image: "https://eckerd.org/wp-content/uploads/2022/01/image-coming-soon.jpg",
+      alt: "Coming soon"
     },
     {
-      link: "https://www.twitter.com",
-      text: "Twitter",
-      image: "https://via.placeholder.com/150"
+      link: "https://github.com/GabrielCorderoDeveloper",
+      text: "Coming soon",
+      image: "https://eckerd.org/wp-content/uploads/2022/01/image-coming-soon.jpg",
+      alt: "Coming soon"
     },
     {
-      link: "https://www.instagram.com",
-      text: "Instagram",
-      image: "https://via.placeholder.com/150"
+      link: "https://github.com/GabrielCorderoDeveloper",
+      text: "Coming soon",
+      image: "https://eckerd.org/wp-content/uploads/2022/01/image-coming-soon.jpg",
+      alt: "Coming soon"
     },
     {
-      link: "https://www.linkedin.com",
-      text: "LinkedIn",
-      image: "https://via.placeholder.com/150"
+      link: "https://github.com/GabrielCorderoDeveloper",
+      text: "Coming soon",
+      image: "https://eckerd.org/wp-content/uploads/2022/01/image-coming-soon.jpg",
+      alt: "Coming soon"
     }
   ];
 
@@ -40,10 +45,14 @@ const Projects = () => {
       {/*//1! Projects card*/}
       <div className="project-container d-flex justify-content-center flex-wrap">
         {data.map((project, index) => (
-          <div key={index} className="project-card col-xl-4 col-md-6 p-4 pb-0" style={{marginTop: `${(index + 1) * 15}px`}}>
-            <div className='m-auto'>
+          <div
+            key={index}
+            className="project-card col-xl-4 col-md-6 p-4 pb-0"
+            style={{ marginTop: `${(index + 1) * 15}px` }}
+          >
+            <div className="m-auto">
               <a href={project.link}>
-                <img className="project-img" src={project.image} alt="" />
+                <img className="project-img" src={project.image} alt={project.alt} />
                 <div className="number-square d-flex align-items-center justify-content-center bg-lightBlack text-clearWhite rounded-4 pt-2">
                   {index + 1}
                 </div>
