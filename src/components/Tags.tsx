52cd6866cbb07b6d6ccceaf39af5358f140a4f6c
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2023-05-27 11:29:36 -0400

    Updated tags.

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -3,8 +3,8 @@ import './Tags.css';
 const Tags = () => {
   return (
     <div className="d-flex flex-wrap col-lg-4 col-md-8 tag-container left-animation ">
-      <div className="tag text-lightBlack bg-secondaryColor">HTML</div>
-      <div className="tag text-clearWhite bg-mainColor">CSS</div>
+      <div className="tag text-lightBlack bg-secondaryColor">HTML5</div>
+      <div className="tag text-clearWhite bg-mainColor">CSS3</div>
       <div className="tag text-clearWhite bg-lightBlack">JavaScript</div>
       <div className="tag text-clearWhite bg-darkBlue">TypeScript</div>
       <div className="tag text-clearWhite bg-lightBlack">Bootstrap</div>
